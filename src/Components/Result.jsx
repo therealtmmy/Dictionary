89ad0c20e44b1810694f70501033477bef4d373e
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-11-05 02:48:16 +0100

    Refactored Code

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -5,58 +5,34 @@ import {FaExternalLinkAlt} from "react-icons/fa";
 const Result = ({value}) => {
   
   return (
-    <div className='Result'>
+  <div className='Result'>
         <h2>{value.word}</h2>
-        <p className='translation'>
-          {value.phonetic}
-        </p>
-
-        <p className='partSpeech'>
-          {value.meanings[0].partOfSpeech}
-        </p>
+        <p className='translation'> {value.phonetic}</p>
+        <p className='partSpeech'> {value.meanings[0].partOfSpeech}</p>
         
-        <div className='line'></div>
+  <div className='line'></div>
         <p>Meaning</p>
-        <ul>
-          <li>{value.meanings[0].definitions[0].definition}</li>
+          <ul>
+              <li>{value.meanings[0].definitions[0].definition}</li>
+              {value.meanings[0] && value.meanings[0].definitions[1] && value.meanings[0].definitions[1].definition ? (<li>{value.meanings[0].definitions[1].definition}</li>) : null}
+              {value.meanings[0] && value.meanings[0].definitions[2] && value.meanings[0].definitions[2].definition ? (<li>{value.meanings[0].definitions[2].definition}</li>) : null}
+          </ul>
 
-          {value.meanings[0] && value.meanings[0].definitions[1] && value.meanings[0].definitions[1].definition ? (<li>{value.meanings[0].definitions[1].definition}</li>) : null}
-
-          {value.meanings[0] && value.meanings[0].definitions[2] && value.meanings[0].definitions[2].definition ? (<li>{value.meanings[0].definitions[2].definition}</li>) : null}
-
-        </ul>
+          {/* Synonyms Session */}
       
-          {value.meanings[0].definitions[0].synonyms.join(", ") && (<p>Synonyms:
-             <span className='Synonyms'> {value.meanings[0].definitions[0].synonyms.join(", ")}
-            </span>
-          </p>)}
+          {value.meanings[0].definitions[0].synonyms.join(", ") && (<p>Synonyms: <span className='Synonyms'> {value.meanings[0].definitions[0].synonyms.join(", ")}</span></p>)}
+          {value.meanings[0].definitions[0].antonyms.join(", ") && (<p>Antonyms:  <span className='Antonyms'> {value.meanings[0].definitions[0].antonyms.join(", ")}</span></p>)}
+          {value.meanings[1] && value.meanings[1].partOfSpeech ? (<p className='partSpeech'> {value.meanings[1].partOfSpeech} </p>) : null}
 
-          {value.meanings[0].definitions[0].antonyms.join(", ") && (<p>Antonyms:  
-            <span className='Antonyms'> {value.meanings[0].definitions[0].antonyms.join(", ")}
-            </span>
-          </p>)}
-          
-          {value.meanings[1] && value.meanings[1].partOfSpeech ? (
-            <p className='partSpeech'>
-              {value.meanings[1].partOfSpeech}
-            </p>
-          ) : null}
-
-          <div className='line'></div>
-           <p>Meaning</p>
+  <div className='line'></div>
+          <p>Meaning</p>
             <ul>
-              {value.meanings[1] && value.meanings[1].definitions[0].definition ? (<li>{value.meanings[1].definitions[0].definition}</li>) : (null)}
-
-              {value.meanings[1] && value.meanings[1].definitions[1] && value.meanings[1].definitions[1].definition ? (<li>{value.meanings[1].definitions[1].definition}</li>) : null}
-
-              {value.meanings[1] && value.meanings[1].definitions[2] && value.meanings[1].definitions[2].definition ? (<li>{value.meanings[1].definitions[2].definition}</li>) : null}
+                {value.meanings[1] && value.meanings[1].definitions[0].definition ? (<li>{value.meanings[1].definitions[0].definition}</li>) : (null)}
+                {value.meanings[1] && value.meanings[1].definitions[1] && value.meanings[1].definitions[1].definition ? (<li>{value.meanings[1].definitions[1].definition}</li>) : null}
+                {value.meanings[1] && value.meanings[1].definitions[2] && value.meanings[1].definitions[2].definition ? (<li>{value.meanings[1].definitions[2].definition}</li>) : null}
             </ul> 
-        
-          <p className='sourceCode'>
-          Source: <a 
-          href={value.sourceUrls}>Wiktionary</a> <FaExternalLinkAlt/>
-          </p>
-    </div>
+          <p className='sourceCode'>Source: <a href={value.sourceUrls}>Wiktionary</a> <FaExternalLinkAlt/> </p>
+  </div>
   )
 }
 
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -7,13 +7,13 @@ const Search = () => {
   const [search, setSearch] = useState("")
   const [content, setContent] = useState([])
 
-  const Submit = (e) => {
-  e.preventDefault()
-  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`)
-  .then(res => res.json())
-  .then(data => setContent(data))
+    const Submit = (e) => {
+    e.preventDefault()
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`)
+    .then(res => res.json())
+    .then(data => setContent(data))
 
-  }
+    }
 
   return (
     <div className='Search'>
@@ -29,10 +29,11 @@ const Search = () => {
     </form>
 
     {Array.isArray(content) ? (content.map((value, index) => <Result key={index} value={value}/>)) 
-    : 
-    (<h2 className='errorMessage'><span style={{fontSize: "4em"}}>😕</span> <br />Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time.</h2>)}
+    : (<h2 className='errorMessage'><span style={{fontSize: "4em"}}>😕</span> 
+    <br />
+    Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again later.</h2>)}
     </div>
-  )
+    )
 }
 
 export default Search
